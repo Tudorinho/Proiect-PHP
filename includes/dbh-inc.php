42a5434d5284a3068149428f85e24d56ff3d3aa1
Tudorinho <dantudor_02@yahoo.com>
2023-01-11 13:38:46 +0200

    Update dbh-inc.php
    Updated dbh-inc.php to remove the localhost, because I host the site now on InfinityFree

--- includes/dbh-inc.php
+++ includes/dbh-inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "phpproject01";
+$servername = "sql306.epizy.com";
+$dBUsername = "epiz_33359487";
+$dBPassword = "BJDkylUy8cF";
+$dBName = "epiz_33359487_databaseproject";
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
